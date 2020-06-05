5017da2e08433ffdfac0c16c2c0730fc5fea6b19
sanjay-sky <san108197@gmail.com>
2020-06-05 22:30:25 +0530

    Angular first program

---
+++ formvalidation/formvalidation.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-formvalidation',
+  templateUrl: './formvalidation.component.html',
+  styleUrls: ['./formvalidation.component.css']
+})
+export class FormvalidationComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
